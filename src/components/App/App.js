4fa7f74865cffd12189f67b7a3b018941e7ca1df
Chris <cjmarsh725@gmail.com>
2019-07-17 12:25:51 -0700

    Added fileStructure state and testing data

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,63 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+class App extends Component {
+  state = {
+    fileStructure: {
+      "/": {
+        indent: -1,
+        path: "/",
+        children: ["/Items", "/Stuff"]
+      },
+      "/ItemsFolder": {
+        name: "ItemsFolder",
+        path: "/ItemsFolder",
+        indent: 0,
+        isOpen: true,
+        parent: "/",
+        children: ["/ItemsFolder/Item1", "/ItemsFolder/Item2"]
+      },
+      "/StuffFolder": {
+        name: "StuffFolder",
+        path: "/StuffFolder",
+        indent: 0,
+        isOpen: false,
+        parent: "/",
+        children: ["/StuffFolder/Stuff1"]
+      },
+      "/ItemsFolder/Item1": {
+        name: "Item1",
+        path: "/ItemsFolder/Item1",
+        indent: 1,
+        isOpen: false,
+        parent: "/ItemsFolder",
+        content: null
+      },
+      "/ItemsFolder/Item2": {
+        name: "Item2",
+        path: "/ItemsFolder/Item2",
+        indent: 1,
+        isOpen: false,
+        parent: "/ItemsFolder",
+        content: null
+      },
+      "/StuffFolder/Stuff1": {
+        name: "Stuff1",
+        path: "/StuffFolder/Stuff1",
+        indent: 1,
+        isOpen: false,
+        parent: "/StuffFolder",
+        content: null
+      }
+    }
+  }
+  
+  render() {
+    return (
+      <div className="App">
+      </div>
+    );
+  }
 }
 
 export default App;
